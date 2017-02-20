1f9f63ff7a58b4498073c211bddb235e4475f9fe
michael <mpgarate@gmail.com>
2017-02-20 14:39:52 -0500

    refactoring get_sequential_pieces_in_coordinate_list

--- src/board.rs
+++ src/board.rs
@@ -51,57 +51,34 @@ impl Board {
             .collect()
     }
 
-    fn get_sequences_in_coordinate_list(
-        &self,
-        coordinates: Vec<(usize, usize)>
-    ) -> Vec<Vec<Piece>>
-        {
-
-        let sequence_clear_len = 4;
-
-        let mut sequences: Vec<Vec<Piece>> = vec![];
-
-        let mut sequence: Vec<Piece> = vec![];
-
-        for (row, col) in coordinates {
-            let color = self.get(row, col);
-
-            let prev_color = match sequence.last() {
-                Some(piece) => Some(piece.color()),
-                _ => None,
-            };
-
-            match (color, prev_color) {
-                (Some(ref c1), Some(ref c2)) if c1 == c2 => {
-                    sequence.push(Piece {row: row, col: col, color: c1.clone() });
-                },
-                (Some(c1), None) => {
-                    if sequence.len() >= sequence_clear_len {
-                        sequences.push(sequence.clone());
+    fn get_sequential_pieces_in_coordinate_list(&self, coordinates: Vec<(usize, usize)>) -> Vec<Piece> {
+        let mut sequence_coordinates: Vec<&(usize, usize)> = coordinates.windows(4)
+            .filter(|sequence| {
+                if let Some(&(row_0, col_0)) = sequence.get(0) {
+                    if let Some(first_color) = self.get(row_0, col_0) {
+                        sequence.iter().all(|&(row, col)| {
+                            self.get(row, col) == Some(first_color.clone())
+                        })
+                    } else {
+                        false
                     }
-
-                    sequence.clear();
-                    sequence.push(Piece {row: row, col: col, color: c1.clone() });
-                },
-                _ => {
-                    if sequence.len() >= sequence_clear_len {
-                        sequences.push(sequence.clone());
-                    }
-
-                    sequence.clear();
+                } else {
+                    false
                 }
-            }
-        }
+            })
+            .flat_map(|sequence| sequence)
+            .collect();
 
-        if sequence.len() >= sequence_clear_len {
-            sequences.push(sequence);
-        }
+            sequence_coordinates.sort();
+            sequence_coordinates.dedup();
 
-        sequences
+            sequence_coordinates.iter().map(|&&(row, col)| {
+                Piece { row: row, col: col, color: self.get(row, col).unwrap() }
+            }).collect()
     }
 
-    pub fn get_sequences_to_clear(&self) -> Vec<Vec<Piece>> {
-        let mut sequences: Vec<Vec<Piece>> = vec![];
+    pub fn get_pieces_to_clear(&self) -> Vec<Piece> {
+        let mut pieces: Vec<Piece> = vec![];
 
         // vertical
         for col in 0..self.width() {
@@ -111,12 +88,12 @@ impl Board {
                 coordinates.push((row, col));
             }
 
-            let new_sequences = self.get_sequences_in_coordinate_list(
+            let new_pieces = self.get_sequential_pieces_in_coordinate_list(
                 coordinates
             );
 
-            for s in new_sequences {
-                sequences.push(s);
+            for s in new_pieces {
+                pieces.push(s);
             }
         }
 
@@ -128,12 +105,12 @@ impl Board {
                 coordinates.push((row, col));
             }
 
-            let new_sequences = self.get_sequences_in_coordinate_list(
+            let new_pieces = self.get_sequential_pieces_in_coordinate_list(
                 coordinates
             );
 
-            for s in new_sequences {
-                sequences.push(s);
+            for s in new_pieces {
+                pieces.push(s);
             }
         }
 
@@ -145,12 +122,12 @@ impl Board {
                 coordinates.push((row, col));
             }
 
-            let new_sequences = self.get_sequences_in_coordinate_list(
+            let new_pieces = self.get_sequential_pieces_in_coordinate_list(
                 coordinates
             );
 
-            for s in new_sequences {
-                sequences.push(s);
+            for s in new_pieces {
+                pieces.push(s);
             }
         }
 
@@ -162,16 +139,16 @@ impl Board {
                 coordinates.push((row, col));
             }
 
-            let new_sequences = self.get_sequences_in_coordinate_list(
+            let new_pieces = self.get_sequential_pieces_in_coordinate_list(
                 coordinates
             );
 
-            for s in new_sequences {
-                sequences.push(s);
+            for s in new_pieces {
+                pieces.push(s);
             }
         }
 
-        sequences
+        pieces
     }
 
     pub fn clear_all(&mut self, pieces: Vec<Piece>) {
@@ -191,7 +168,7 @@ mod tests {
     use game::Piece;
 
     #[test]
-    fn get_sequences_to_clear_diagonal_up() {
+    fn get_pieces_to_clear_diagonal_up() {
         let board = Board {
             color_matrix: vec![
                 vec![ None, None, None, Some(Red) ],
@@ -201,23 +178,23 @@ mod tests {
             ],
         };
 
-        let mut expected_sequences_to_clear = vec![vec![
+        let mut expected_pieces_to_clear = vec![
             Piece { row: 3, col: 0, color: Red },
             Piece { row: 2, col: 1, color: Red },
             Piece { row: 1, col: 2, color: Red },
             Piece { row: 0, col: 3, color: Red },
-        ]];
+        ];
 
-        let mut sequences_to_clear = board.get_sequences_to_clear();
+        let mut pieces_to_clear = board.get_pieces_to_clear();
 
-        expected_sequences_to_clear.sort();
-        sequences_to_clear.sort();
+        expected_pieces_to_clear.sort();
+        pieces_to_clear.sort();
 
-        assert_eq!(expected_sequences_to_clear, sequences_to_clear);
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
     }
 
     #[test]
-    fn get_sequences_to_clear_diagonal_up_off_center_up() {
+    fn get_pieces_to_clear_diagonal_up_off_center_up() {
         let board = Board {
             color_matrix: vec![
                 vec![ None, None, None, Some(Red), None ],
@@ -228,23 +205,23 @@ mod tests {
             ],
         };
 
-        let mut expected_sequences_to_clear = vec![vec![
+        let mut expected_pieces_to_clear = vec![
             Piece { row: 3, col: 0, color: Red },
             Piece { row: 2, col: 1, color: Red },
             Piece { row: 1, col: 2, color: Red },
             Piece { row: 0, col: 3, color: Red },
-        ]];
+        ];
 
-        let mut sequences_to_clear = board.get_sequences_to_clear();
+        let mut pieces_to_clear = board.get_pieces_to_clear();
 
-        expected_sequences_to_clear.sort();
-        sequences_to_clear.sort();
+        expected_pieces_to_clear.sort();
+        pieces_to_clear.sort();
 
-        assert_eq!(expected_sequences_to_clear, sequences_to_clear);
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
     }
 
     #[test]
-    fn get_sequences_to_clear_diagonal_up_off_center_down() {
+    fn get_pieces_to_clear_diagonal_up_off_center_down() {
         let board = Board {
             color_matrix: vec![
                 vec![ None, None, None, None, None ],
@@ -255,23 +232,23 @@ mod tests {
             ],
         };
 
-        let mut expected_sequences_to_clear = vec![vec![
+        let mut expected_pieces_to_clear = vec![
             Piece { row: 4, col: 1, color: Red },
             Piece { row: 3, col: 2, color: Red },
             Piece { row: 2, col: 3, color: Red },
             Piece { row: 1, col: 4, color: Red },
-        ]];
+        ];
 
-        let mut sequences_to_clear = board.get_sequences_to_clear();
+        let mut pieces_to_clear = board.get_pieces_to_clear();
 
-        expected_sequences_to_clear.sort();
-        sequences_to_clear.sort();
+        expected_pieces_to_clear.sort();
+        pieces_to_clear.sort();
 
-        assert_eq!(expected_sequences_to_clear, sequences_to_clear);
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
     }
 
     #[test]
-    fn get_sequences_to_clear_horizontal() {
+    fn get_pieces_to_clear_horizontal() {
         let board = Board {
             color_matrix: vec![
                 vec![ None, None, None, None ],
@@ -281,20 +258,20 @@ mod tests {
             ],
         };
 
-        let expected_sequences_to_clear = vec![vec![
+        let expected_pieces_to_clear = vec![
             Piece { row: 2, col: 0, color: Red },
             Piece { row: 2, col: 1, color: Red },
             Piece { row: 2, col: 2, color: Red },
             Piece { row: 2, col: 3, color: Red },
-        ]];
+        ];
 
-        let sequences_to_clear = board.get_sequences_to_clear();
+        let pieces_to_clear = board.get_pieces_to_clear();
 
-        assert_eq!(expected_sequences_to_clear, sequences_to_clear);
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
     }
 
     #[test]
-    fn get_sequences_to_clear_vertical() {
+    fn get_pieces_to_clear_vertical() {
         let board = Board {
             color_matrix: vec![
                 vec![ Some(Red), None ],
@@ -305,20 +282,20 @@ mod tests {
             ],
         };
 
-        let expected_sequences_to_clear = vec![vec![
+        let expected_pieces_to_clear = vec![
             Piece { row: 0, col: 0, color: Red },
             Piece { row: 1, col: 0, color: Red },
             Piece { row: 2, col: 0, color: Red },
             Piece { row: 3, col: 0, color: Red },
-        ]];
+        ];
 
-        let sequences_to_clear = board.get_sequences_to_clear();
+        let pieces_to_clear = board.get_pieces_to_clear();
 
-        assert_eq!(expected_sequences_to_clear, sequences_to_clear);
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
     }
 
     #[test]
-    fn get_sequences_to_clear_vertical_none_eligible() {
+    fn get_pieces_to_clear_vertical_none_eligible() {
         let board = Board {
             color_matrix: vec![
                 vec![ Some(Red), None ],
@@ -328,11 +305,11 @@ mod tests {
             ],
         };
 
-        let expected_sequences_to_clear: Vec<Vec<Piece>> = vec![];
+        let expected_pieces_to_clear: Vec<Piece> = vec![];
 
-        let sequences_to_clear = board.get_sequences_to_clear();
+        let pieces_to_clear = board.get_pieces_to_clear();
 
-        assert_eq!(expected_sequences_to_clear, sequences_to_clear);
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
     }
 
     #[test]
