db4d9dc1a8e4d7f4c6d3c38881477d246218c5af
michael <mpgarate@gmail.com>
2017-02-20 15:01:50 -0500

    clear diagonal down

--- src/board.rs
+++ src/board.rs
@@ -151,6 +151,21 @@ impl Board {
             }
         }
 
+        // diagonal down by col, looks like \
+        for starting_col in 1..self.width() {
+            let mut coordinates = vec![];
+
+            for (row, col) in (0..self.height()).zip((starting_col..self.width())) {
+                coordinates.push((row, col));
+            }
+
+            let new_pieces = self.get_sequential_pieces(coordinates);
+
+            for s in new_pieces {
+                pieces.push(s);
+            }
+        }
+
         pieces
     }
 
@@ -186,6 +201,60 @@ mod tests {
             Piece { row: 1, col: 1, color: Red },
             Piece { row: 2, col: 2, color: Red },
             Piece { row: 3, col: 3, color: Red },
+        ];
+
+        let mut pieces_to_clear = board.get_pieces_to_clear();
+
+        expected_pieces_to_clear.sort();
+        pieces_to_clear.sort();
+
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
+    }
+
+    #[test]
+    fn get_pieces_to_clear_diagonal_down_off_center_down() {
+        let board = Board {
+            color_matrix: vec![
+                vec![ None, None, None, None, None],
+                vec![ Some(Red), None, None, None ],
+                vec![ None, Some(Red), None, None ],
+                vec![ None, None, Some(Red), None ],
+                vec![ None, None, None, Some(Red) ],
+            ],
+        };
+
+        let mut expected_pieces_to_clear = vec![
+            Piece { row: 1, col: 0, color: Red },
+            Piece { row: 2, col: 1, color: Red },
+            Piece { row: 3, col: 2, color: Red },
+            Piece { row: 4, col: 3, color: Red },
+        ];
+
+        let mut pieces_to_clear = board.get_pieces_to_clear();
+
+        expected_pieces_to_clear.sort();
+        pieces_to_clear.sort();
+
+        assert_eq!(expected_pieces_to_clear, pieces_to_clear);
+    }
+
+    #[test]
+    fn get_pieces_to_clear_diagonal_down_off_center_right() {
+        let board = Board {
+            color_matrix: vec![
+                vec![ None, None, None, None, None ],
+                vec![ None, Some(Red), None, None, None ],
+                vec![ None, None, Some(Red), None, None ],
+                vec![ None, None, None, Some(Red), None ],
+                vec![ None, None, None, None, Some(Red) ],
+            ],
+        };
+
+        let mut expected_pieces_to_clear = vec![
+            Piece { row: 1, col: 1, color: Red },
+            Piece { row: 2, col: 2, color: Red },
+            Piece { row: 3, col: 3, color: Red },
+            Piece { row: 4, col: 4, color: Red },
         ];
 
         let mut pieces_to_clear = board.get_pieces_to_clear();
